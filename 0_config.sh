b76f48cdd2da9e4f7d9cc260cf77ab16bb3df8ba
dronga <alexd@well.ox.ac.uk>
2016-11-09 11:49:02 +0000

    Update 0_config.sh

--- 0_config.sh
+++ 0_config.sh
@@ -1,7 +1,13 @@
 PLINK=~/bin/plink-270116
 GTOOL=/apps/well/gtool/0.7.5/gtool
 
-INPUT=/well/lindgren/alexd/HAPMAP/SHAPEIT/MERGED_combined
+#INPUT is the full path + file root to the .ped and .map files split by chr
+INPUT=/well/drong/POETEST/INPUT/hapmap3_r2_updateb37_fwd.MERGED
+
 ANNO=/well/lindgren/alexd/miRNA/annotation.RData
+
+# Expression data set
 EXP=/well/lindgren/alexd/miRNA/expression.RData
-OUTPUT=/well/lindgren/alexd/miRNA/HAPMAP_SHAPEIT/OUTPUT
+
+# OUTPUT filepath (without / at the end)
+OUTPUT=/well/drong/POETEST/OUTPUT
