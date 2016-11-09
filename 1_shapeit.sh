7c0a9915ce6fc70e3c1a50b9438e3cc2860676cb
dronga <alexd@well.ox.ac.uk>
2016-11-09 10:43:41 +0000

    Update 1_shapeit.sh

--- 1_shapeit.sh
+++ 1_shapeit.sh
@@ -22,12 +22,14 @@ if [ -z "$SGE_TASK_ID" ]; then SGE_TASK_ID=$1; fi
 
 CHR=$SGE_TASK_ID
 
-$SHAPEIT --input-ped /well/lindgren/alexd/HAPMAP/hapmap3_pop/hapmap3_r2_updateb37_fwd.MERGED.chr${CHR}.ped /well/lindgren/alexd/HAPMAP/hapmap3_pop/hapmap3_r2_updateb37_fwd.MERGED.chr${CHR}.map \
+source 0_config.sh
+
+$SHAPEIT --input-ped ${INPUT}.chr${CHR}.ped {INPUT}.chr${CHR}.map \
         -M /well/lindgren/alexd/1000GP_Phase3/genetic_map_chr${CHR}_combined_b37.txt \
         --duohmm \
         -W 5 \
-        -O /well/lindgren/alexd/HAPMAP/SHAPEIT/MERGED_chr${CHR}
-        --exclude-snp /well/lindgren/alexd/HAPMAP/hapmap3_pop/hapmap3_r2_updateb37_fwd.MERGED.chr${CHR}.dupvar
+        -O ${OUTPUT}.chr${CHR}
+        #--exclude-snp /well/lindgren/alexd/HAPMAP/hapmap3_pop/hapmap3_r2_updateb37_fwd.MERGED.chr${CHR}.dupvar
         #--output-max  \
         #--output-graph /panfs/panasas01/sscm/xt610/TRIOS/IMPUTE/SHAPEIT/TRIO-graph
 
